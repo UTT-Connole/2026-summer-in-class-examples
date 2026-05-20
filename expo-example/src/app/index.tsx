ed54e0a17d42f70fc4c792beb076e7dd9147e028
Brayden Connole <brayden.connole@gmail.com>
2026-05-19 20:05:04 -0600

    needs updating

--- expo-example/src/app/index.tsx
+++ expo-example/src/app/index.tsx
@@ -1,9 +1,23 @@
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+
+  let studentName = "Corbin"
+
+  let updateStudentName = () => {
+    console.log(studentName)
+    studentName = "Joseph"
+    console.log(studentName)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Hello Class!</Text>
+      <Text onPress={updateStudentName}>Click on Me!</Text>
+      <Text>Hello {studentName}</Text>
+      <Text>Hello Class!</Text>
+      <Text>Hello Class!</Text>
+      <Text>Hello Class!</Text>
     </View>
   );
 }
